c3ea1e50bc0020bfe710a1541b27191ca5b7e605
Pau Bonilla <paulo111995@gmail.com>
2020-09-06 22:30:13 -0700

    added search bar

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia, Typography, TextField } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import { toFirstCharUppercase } from '../constants';
+import SearchIcon from "@material-ui/icons/Search";
 import axios from 'axios';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   pokedexContainer: {
     paddingTop: '20px',
     paddingLeft: '50px',
@@ -15,13 +16,30 @@ const useStyles = makeStyles({
   },
   cardContent: {
     textAlign: 'center'
-  }
-})
+  },
+  searchContainer: {
+    display: "flex",
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    marginTop: "5px",
+    marginBottom: "5px",
+  },
+  searchIcon: {
+    alignSelf: "flex-end",
+    marginBottom: "5px",
+  },
+  searchInput: {
+    width: "200px",
+    margin: "5px",
+  },
+}));
 
 export default function Pokedex(props) {
   const { history } = props;
   const classes = useStyles();
   const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     axios
@@ -41,6 +59,10 @@ export default function Pokedex(props) {
         setPokemonData(newPokemonData);
       });
   }, []);
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   const getPokemonCard = (pokemonId) => {
     const { id, name, sprite } = pokemonData[`${pokemonId}`]
@@ -64,11 +86,22 @@ export default function Pokedex(props) {
   return (
     <>
       <AppBar position="static">
-        <Toolbar />
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField
+              className={classes.searchInput}
+              onChange={handleSearchChange}
+              label="Pokemon"
+              variant="standard"
+            />
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
           {Object.keys(pokemonData).map((pokemonId) =>
+            pokemonData[pokemonId].name.includes(filter) &&
             getPokemonCard(pokemonId)
           )}
         </Grid>
