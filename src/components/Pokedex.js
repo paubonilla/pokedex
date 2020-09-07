8a3616d0084a5045177592531d6816975d7ce6dc
Pau Bonilla <paulo111995@gmail.com>
2020-09-06 20:25:05 -0700

    modified pokedex

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: '20px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+  }
+})
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>Yo</CardContent>
+      </Card>
+    </Grid>
+  )
+}
 
 export default function Pokedex() {
+  const classes = useStyles();
+
   return (
-    <div>
-      This is a pokedex page
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+      </Grid>
+    </>
   )
 }
